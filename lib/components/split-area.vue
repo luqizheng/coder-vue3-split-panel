e552f196ff7a39094d3485c66686d26e6cf1f17c
leo.lu <lqz.leo@gmail.com>
2022-11-21 17:05:42 +0800

    更改定义

--- lib/components/split-area.vue
+++ lib/components/split-area.vue
@@ -3,26 +3,55 @@
     <slot></slot>
   </div>
 </template>
-
-<script lang="ts" setup>
-import { computed, getCurrentInstance } from 'vue';
-const props = defineProps({
-  size: {
-    type: Number,
-    default: 50,
+<script lang="ts">
+import { getCurrentInstance, computed, defineComponent, SetupContext } from 'vue';
+export default defineComponent({
+  name: "SplitArea",
+  props: {
+    size: {
+      type: Number,
+      default: 50,
+    },
+    minSize: {
+      type: Number,
+      default: 100,
+    },
   },
-  minSize: {
-    type: Number,
-    default: 100,
+  setup(props, ctx: SetupContext) {
+
   },
+  computed: {
+    classes(): string {
+      var parent = getCurrentInstance()?.parent;
+      var a = parent?.props;
+      return `split split-` + a?.direction;
+    },
+  }
+
 })
-
-const classes = computed(() => {
-  var parent = getCurrentInstance()?.parent;
-  var a = parent?.props;
-  return `split split-` + a?.direction;
-})
-
-
-
 </script>
+<!-- <script lang="ts">
+//import Emitter from "../mixins/emitter";
+import { getCurrentInstance } from "vue";
+export default {
+  name: "SplitArea",
+  //mixins: [Emitter],
+  props: {
+    size: {
+      type: Number,
+      default: 50,
+    },
+    minSize: {
+      type: Number,
+      default: 100,
+    },
+  },
+  computed: {
+    classes(): string {
+      var parent = getCurrentInstance()?.parent;
+      var a = parent?.props;
+      return `split split-` + a?.direction;
+    },
+  },
+};
+</script> -->
