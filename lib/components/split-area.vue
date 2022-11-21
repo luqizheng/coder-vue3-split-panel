38c9e0001e735be46be8ba0c1a7e641efc94e45f
leo.lu <lqz.leo@gmail.com>
2022-11-21 15:36:15 +0800

    some bug fix

--- lib/components/split-area.vue
+++ lib/components/split-area.vue
@@ -4,28 +4,25 @@
   </div>
 </template>
 
-<script lang="ts">
-//import Emitter from "../mixins/emitter";
-import { getCurrentInstance } from "vue";
-export default {
-  name: "SplitArea",
-  //mixins: [Emitter],
-  props: {
-    size: {
-      type: Number,
-      default: 50,
-    },
-    minSize: {
-      type: Number,
-      default: 100,
-    },
+<script lang="ts" setup>
+import { computed, getCurrentInstance } from 'vue';
+const props = defineProps({
+  size: {
+    type: Number,
+    default: 50,
   },
-  computed: {
-    classes(): string {
-      var parent = getCurrentInstance()?.parent;
-      var a = parent?.props;
-      return `split split-` + a?.direction;
-    },
+  minSize: {
+    type: Number,
+    default: 100,
   },
-};
+})
+
+const classes = computed(() => {
+  var parent = getCurrentInstance()?.parent;
+  var a = parent?.props;
+  return `split split-` + a?.direction;
+})
+
+
+
 </script>
